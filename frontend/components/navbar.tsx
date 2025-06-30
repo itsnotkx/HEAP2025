97b41ec336602df5cfdde37c1218a62fc1b8dd37
chaw678 <eichawzin123@gmail.com>
2025-06-30 22:28:10 +0800

    FEAT: About Us page

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -49,70 +49,12 @@ export default function Navigationbar({ shouldHideOnScroll = true }: Navigationb
       </NavbarContent>
       <nav className="flex space-x-40 text-lg font-semibold">
         <a href="#" className="hover:text-primary">Features</a>
-        <a href="#" className="hover:text-primary">About Us</a>
+        <a href="/aboutUs" className="hover:text-primary">About Us</a>
         <a href="#" className="hover:text-primary">Support</a>
       </nav>
       <Button color="primary" className="text-white px-10 ml-40 py-2 rounded-full" size="lg" onPress={handleLoginClick}>
         Login
       </Button>
-
-      {/* <NavbarContent className="hidden sm:flex gap-4 flex-1 items-start mt-8" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="/home" className="text-base leading-tight py-0 my-0">
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link color="foreground" href="/customers" className="text-base leading-tight py-0 my-0">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="warning" href="/surprise" className="text-base leading-tight py-0 my-0">
-            Surprise Me!
-          </Link>
-        </NavbarItem>
-      </NavbarContent> */}
-
-      {/* <NavbarContent as="div" justify="end" className="items-start mt-6">
-        <Button
-          as={Link}
-          href="../app/bookmarks"
-          aria-label="Bookmark"
-          style={{
-            background: "none",
-            border: "none",
-            cursor: "pointer",
-            padding: 0,
-          }}
-        >
-          <FaBookmark className="w-6 h-6" />
-        </Button>
-
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile">
-              <p className="font-semibold">Account</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent> */}
     </Navbar>
   );
 }
