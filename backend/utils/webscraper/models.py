ab522c8cf45c2337591d4e80c219b1a54c524cb1
Ethan <limjinethan@gmail.com>
2025-07-10 16:48:31 +0800

    Changed DB parameters

--- backend/utils/webscraper/models.py
+++ backend/utils/webscraper/models.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class Event(BaseModel):
     title: str
@@ -8,10 +8,15 @@ class Event(BaseModel):
     time: Optional[str] = None
     location: Optional[str]
     postal_code: Optional[str]
-    category: Optional[str]
+    category: Optional[float]
     price: Optional[float]
     description: str
     image_urls: List[str]
     organizer: Optional[str]
     official_link: Optional[str]
     url: List[str]
+    @field_validator('category')
+    def round_category(cls, v):
+        if v is not None:
+            return round(float(v), 1)
+        return v
