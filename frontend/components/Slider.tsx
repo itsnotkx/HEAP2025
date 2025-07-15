1bd8db40b419bd258e80794fc200cd16fef6e01d
Matthewkhlr <matthewkhoolr@gmail.com>
2025-07-15 22:01:40 +0800

    Edited Slider component to standardize and centralize image

--- frontend/components/Slider.tsx
+++ frontend/components/Slider.tsx
@@ -1,33 +1,65 @@
 "use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import Image from 'next/image';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Slider() {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      spaceBetween={0}
-      centeredSlides={true}
-      slidesPerView={1}
-      speed={2000}
-      autoplay={{
-        delay:4000,
-        disableOnInteraction: false,
-      }}
-      navigation
-      pagination={{ clickable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide><Image src="/images/sg_luge.png" alt="slide 1" width={800} height={600} style={{ objectFit: 'contain' }}></Image></SwiperSlide>
-      <SwiperSlide><Image src="/images/sg_rollercoaster.png" alt="slide 2" width={800} height={600}></Image></SwiperSlide>
-      <SwiperSlide><Image src="/images/sg_zoo.png" alt="slide 3" width={800} height={600}></Image></SwiperSlide>
-      <SwiperSlide><Image src="/images/sg_nightclub.png" alt="slide 4" width={800} height={600}></Image></SwiperSlide>
-    </Swiper>
+    <div className="w-full flex justify-center">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        centeredSlides={true}
+        slidesPerView={1}
+        speed={2000}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        style={{
+          width: "800px",   // fixed container width
+          height: "600px",  // fixed container height
+        }}
+      >
+        {/** Wrap each image in a div to make layout easier */}
+        {[
+          "/images/sg_luge.png",
+          "/images/sg_rollercoaster.png",
+          "/images/sg_zoo.png",
+          "/images/sg_nightclub.png",
+        ].map((img, index) => (
+          <SwiperSlide
+            key={index}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+              backgroundColor: "#000", // Optional: dark background
+            }}
+          >
+     
+            <Image
+              src={img}
+              alt={`slide ${index + 1}`}
+              width={800}
+              height={600}
+              style={{
+                objectFit: "fill",
+              }}
+            />
+
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
